6c3325234e4861e5ceb1756ec84803a11b0796f3
Tamanna Dubey <155536196+TamannaDubey@users.noreply.github.com>
2025-07-17 22:51:28 +0530

    Delete main.js.js

--- main.js.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["an aspiring AIML &amp; Web Developer ", " a Passionate Learner"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
