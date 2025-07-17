3120397f6fc0390efaae718611475d001e4761ae
Tamanna Dubey <155536196+TamannaDubey@users.noreply.github.com>
2025-07-17 20:06:02 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["an aspiring AIML &amp; Web Developer ", " a Passionate Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
